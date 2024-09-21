5a8f7a1517f7a66ea54693efc2a499c675813c75
Giovanna Prates <uma.prates@gmail.com>
2024-09-21 15:57:48 -0300

    começando metas

---
+++ index.js
@@ -0,0 +1,17 @@
+let meta = {
+    value: "estudar 1h por dia",
+    checked: false,
+}
+
+let metas= [
+    meta,
+    { 
+        value: "fazer exercício todos os dias",
+        checked: false
+    }
+]
+ 
+
+
+console.log (meta.value)
+
